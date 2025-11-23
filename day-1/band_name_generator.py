e98b25095fbe8fe3a5849e9ec986ef6de35da75e
Marcel van Leeuwen <marcel@van-leeuwen.email>
2025-11-23 10:26:19 +0100

    chore: update day-1

---
+++ day-1/band_name_generator.py
@@ -0,0 +1,6 @@
+# Create a program called band_name_generator using input, print, and variables.
+print("Welcome to the Band Name Generator.")
+city = input("Hi there, which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name should be " + city + " " + pet)
+

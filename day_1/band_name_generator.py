71f521308e076ac9a8ba50680a66ca1d4dd1f172
Marcel <marcel@van-leeuwen.email>
2025-11-23 10:33:42 +0100

    Delete day_1 directory

--- day_1/band_name_generator.py
+++
@@ -1,6 +0,0 @@
-# Create a program called band_name_generator using input, print, and variables.
-print("Welcome to the Band Name Generator.")
-city = input("Hi there, which city did you grow up in?\n")
-pet = input("What is the name of a pet?\n")
-print("Your band name should be " + city + " " + pet)
-
